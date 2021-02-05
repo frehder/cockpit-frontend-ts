39c685fb3a76ad81505fc9b4aff71a32829f909d
frehder <hi@florianrehder.de>
2021-02-05 14:41:44 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (): React.ReactElement => {
+    return (
+        <div className="App">
+            <p>New website</p>
+        </div>
+    );
+};
+
+export default App;
