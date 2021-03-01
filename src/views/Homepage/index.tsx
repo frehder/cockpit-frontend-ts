3226ed1bb85c751375627267e312a46bfee04a76
frehder <hi@florianrehder.de>
2021-03-01 19:51:18 +0100

    update homepage view

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Status } from '../../types/store';
+import { getHomepage } from '../../store/homepage/slices';
+import { selectBlock, selectStatus } from '../../store/homepage/selectors';
+import FullscreenLoader from '../FullscreenLoader';
 
 const Homepage = (): React.ReactElement => {
+    const dispatch = useDispatch();
+    const [ready, setReady] = React.useState<boolean>(false);
+    const pageStatus = useSelector(selectStatus);
+    React.useEffect(() => {
+        setReady(pageStatus === Status.LOADED);
+        if (pageStatus !== Status.LOADED) dispatch(getHomepage);
+    }, [pageStatus]);
+
+    if (!ready) return <FullscreenLoader />;
+
     return (
         <section>
             <p>views/Homepage</p>
