929c196f45ae43e2f1d77291955e1f546224d4e4
frehder <hi@florianrehder.de>
2021-03-01 19:43:24 +0100

    add app boot

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 const App = (): React.ReactElement => {
+    const [ready, setReady] = React.useState<boolean>(false);
+
+    const boot = async () => {
+        try {
+        } catch (error) {
+            console.error('app boot error:', error);
+        } finally {
+            setReady(true);
+        }
+    };
+
+    React.useEffect(() => {
+        boot();
+    }, []);
+
     return (
     );
 };
+
+export default App;
